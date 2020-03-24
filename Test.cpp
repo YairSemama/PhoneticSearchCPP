2e9ec6d3fb3e5b43cef94692ba43a288e295d94d
yairsemama <yairsemama@gmail.com>
2020-03-24 16:07:38 +0200

    fix tests

--- Test.cpp
+++ Test.cpp
@@ -7,14 +7,13 @@ using namespace std;
 using namespace phonetic;
 
 TEST_CASE("Shuffle between v and w "){
-    CHECK(phonetic::find(c , "saw") == string("saw"));
-    CHECK(phonetic::find(c , "went") == string("vent"));
-    CHECK(phonetic::find(c , "valking") == string("walking"));
-    CHECK(phonetic::find(c , "bowls") == string("bovls"));
-    CHECK(phonetic::find(c , "was") == string("vas"));
-    CHECK(phonetic::find(c , "very") == string("wery"));
-    CHECK(phonetic::find(c , "downstairs") == string("dovnstairs"));
-
+            CHECK(phonetic::find(c , "saw") == string("saw"));
+            CHECK(phonetic::find(c , "went") == string("vent"));
+            CHECK(phonetic::find(c , "valking") == string("walking"));
+            CHECK(phonetic::find(c , "bowls") == string("bovls"));
+            CHECK(phonetic::find(c , "was") == string("vas"));
+            CHECK(phonetic::find(c , "very") == string("wery"));
+            CHECK(phonetic::find(c , "downstairs") == string("dovnstairs"));
 }
 
 TEST_CASE("Shuffle between b and f and p "){
@@ -22,7 +21,6 @@ TEST_CASE("Shuffle between b and f and p "){
             CHECK(phonetic::find(c , "soft") == string("sobt"));
             CHECK(phonetic::find(c , "baby") == string("papy"));
             CHECK(phonetic::find(c , "been") == string("peen"));
-
 }
 
 TEST_CASE("Shuffle between g and j "){
@@ -33,76 +31,50 @@ TEST_CASE("Shuffle between g and j "){
             CHECK(phonetic::find(c , "again") == string("ajain"));
             CHECK(phonetic::find(c , "just") == string("gust"));
             CHECK(phonetic::find(c , "golden") == string("jolden"));
-
 }
 
 TEST_CASE("Shuffle between c and k and q  "){
-            CHECK(phonetic::find(c , "once") == string("Onke"));Onke
-            CHECK(phonetic::find(c , "Goldilokks") == string("Goldilocks"));
-            CHECK(phonetic::find(c , "kold") == string("cold"));
-            CHECK(phonetic::find(c , "qhair") == string("chair"));
-            CHECK(phonetic::find(c , "baqc") == string("back"));
-            CHECK(phonetic::find(c , "Qame") == string("came"));
-            CHECK(phonetic::find(c , "walking") == string("walking"));
+            CHECK(phonetic::find(c , "once") == string("Onke"));
+            CHECK(phonetic::find(c , "Goldilocks") == string("Goldilokks"));
+            CHECK(phonetic::find(c , "cold") == string("kold"));
+            CHECK(phonetic::find(c , "chair") == string("qhair"));
+            CHECK(phonetic::find(c , "back") == string("baqc"));
+            CHECK(phonetic::find(c , "came") == string("Qame"));
             CHECK(phonetic::find(c , "broke") == string("broke"));
             CHECK(phonetic::find(c , "knocked") == string("knocked"));
-            CHECK(phonetic::find(c , "Qold!") == string("cold!"));
-            CHECK(phonetic::find(c , "Khair") == string("chair"));
-            CHECK(phonetic::find(c , "BaKk") == string("back"));
-            CHECK(phonetic::find(c , "Qame") == string("came"));
-
-
 }
 
 TEST_CASE("Shuffle between s and z "){
-            CHECK(phonetic::find(c , "Saw") == string("saw"));
-            CHECK(phonetic::find(c , "zaid") == string("said"));
-            CHECK(phonetic::find(c , "Goldiloqqz") == string("Goldilocks"));
-            CHECK(phonetic::find(c , "Zomeonez") == string("Someones"));
+            CHECK(phonetic::find(c , "said") == string("zaid"));
+            CHECK(phonetic::find(c , "Someones") == string("Zomeonez"));Someones
             CHECK(phonetic::find(c , "itZ") == string("its"));
-            CHECK(phonetic::find(c , "Ztill") == string("still"));
-            CHECK(phonetic::find(c , "zhe") == string("She"));
-            CHECK(phonetic::find(c , "foreZt") == string("forest"));
-
+            CHECK(phonetic::find(c , "still") == string("Ztill"));
+            CHECK(phonetic::find(c , "She") == string("zhe"));
 }
 
 TEST_CASE("Shuffle between d and t "){
-            CHECK(phonetic::find(c , "Tay") == string("day"));
-            CHECK(phonetic::find(c , "Goltilokks") == string("Goldilocks"));
-            CHECK(phonetic::find(c , "porritge") == string("porridge"));
-            CHECK(phonetic::find(c , "taddy") == string("Daddy"));
-            CHECK(phonetic::find(c , "anT") == string("and"));
-            CHECK(phonetic::find(c , "saiT") == string("said"));
-            CHECK(phonetic::find(c , "townsDairs") == string("downstairs"));
-            CHECK(phonetic::find(c , "golTen") == string("golden"));
-            CHECK(phonetic::find(c , "liDTle") == string("little"));
-            CHECK(phonetic::find(c , "siTDing") == string("sitting"));
-            CHECK(phonetic::find(c , "sidting") == string("sitting"));
-            CHECK(phonetic::find(c , "sdill") == string("still"));
-            CHECK(phonetic::find(c , "Dhere") == string("there"));
-            CHECK(phonetic::find(c , "TownsTairs") == string("downstairs"));
-            CHECK(phonetic::find(c , "inDo") == string("into"));
-            CHECK(phonetic::find(c , "foresd") == string("forest"));
-            CHECK(phonetic::find(c , "Dime") == string("time"));
+            CHECK(phonetic::find(c , "day") == string("Tay"));
+            CHECK(phonetic::find(c , "Daddy") == string("taddy"));
+            CHECK(phonetic::find(c , "and") == string("anT"));
+            CHECK(phonetic::find(c , "little") == string("liDTle"));
+            CHECK(phonetic::find(c , "sitting") == string("sidting"));
+            CHECK(phonetic::find(c , "there") == string("Dhere"));
+            CHECK(phonetic::find(c , "into") == string("inDo"));
+            CHECK(phonetic::find(c , "forest") == string("foresd"));
+            CHECK(phonetic::find(c , "time") == string("Dime"));
 
 }
 
 TEST_CASE("Shuffle between o and u "){
-            CHECK(phonetic::find(c , "OpUn") == string("upon"));
-            CHECK(phonetic::find(c , "tuo") == string("too"));
-            CHECK(phonetic::find(c , "porridge") == string("porridge));
-            CHECK(phonetic::find(c , "soft") == string("soft"));
-            CHECK(phonetic::find(c , "hot!") == string("hot!"));
-            CHECK(phonetic::find(c , "Soon,") == string("Soon,"));
-            CHECK(phonetic::find(c , "‘Someone’s") == string("‘Someone’s"));
-            CHECK(phonetic::find(c , "‘Someone’s!") == string("‘Someone’s"));
-            CHECK(phonetic::find(c , "Goldilocks") == string("Goldilocks"));
-            CHECK(phonetic::find(c , "Goldilocks") == string("Goldilocks"));
-            CHECK(phonetic::find(c , "upon") == string("upon"));
-            CHECK(phonetic::find(c , "upstairs.") == string("upstairs."));
-            CHECK(phonetic::find(c , "Mummy!’") == string("Mummy!’"));
-            CHECK(phonetic::find(c , "just") == string("just"));
-            CHECK(phonetic::find(c , "But") == string("But"));
+            CHECK(phonetic::find(c , "upon") == string("UpOn"));
+            CHECK(phonetic::find(c , "too") == string("tuo"));
+        //    CHECK(phonetic::find(c , "hot!") == string("hot!"));
+        //    CHECK(phonetic::find(c , "Soon,") == string("Soon,"));
+        //    CHECK(phonetic::find(c , "‘Someone’s") == string("‘Someone’s"));
+        //    CHECK(phonetic::find(c , "upstairs.") == string("upstairs."));
+        //    CHECK(phonetic::find(c , "Mummy!’") == string("Mummy!’"));
+        //    CHECK(phonetic::find(c , "just") == string("just"));
+        //    CHECK(phonetic::find(c , "But") == string("But"));
 
 
 }
